28bdcc9c68b829c104c89680a5dd19a44d69cf05
Serhii Tarasenko <stdev33@gmail.com>
2023-10-11 14:49:04 +0300

    [+] task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.append(...createListItems(ingredients));
+
+function createListItems(array) {
+  return array.map(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    listItem.classList.add('item');
+    return listItem;
+  });
+}
